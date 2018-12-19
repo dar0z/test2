7bd1feb1077ded4e11c99ec5f3ef0bb68acb772f
student <darko1601@gmail.com>
2018-12-19 01:42:09 +0100

    first

---
+++ src/test/java/RangeTest.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RangeTest {
+    @Test
+    public void shouldSayThat15IsInRange() {
+        Range range = new Range(10, 20);
+        Assert.assertTrue(range.isInRange(15));
+    }
+}
